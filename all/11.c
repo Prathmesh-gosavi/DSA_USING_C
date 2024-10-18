f20799c4cc6e5c0f88fce9111d7f19aeb71e964a
Prathmesh-gosavi <gosaviprathmesh2420@gmail.com>
2024-10-19 00:21:10 +0530

    all

--- all/11.c
+++ all/11.c
@@ -21,6 +21,10 @@ trapezoidalf(f, 0, 1, 5)
 OUTPUT:
 1.5
 5 𝑑𝑥
+
+
+
+
 Q.2 find the value of ∫0
 1+𝑥 2
 using trapezoidal rule, take h=0.5.
@@ -42,6 +46,11 @@ trapezoidal(f,0,5,10)
 Integration by Trap is: 17.5
 41Dr. D. Y. Patil ACS College, Pimpri, Pune – 18.
 10
+
+
+
+
+
 Q.3 Solve the integral ∫0 (𝑥 + 1)3 𝑑𝑥 using trapezoidal rule, take h=1.
 Program:
 def trapezoidal(f,a,b,n):
@@ -60,6 +69,10 @@ OUTPUT:
 trapezoidal(f,0,10,10)
 Integration by trap is: 60.0
 1.4
+
+
+
+
 Q.4 Solve the integral ∫0.2 (sinx − log 𝑥 + 𝑒 𝑥 )𝑑𝑥 using trapezoidal rule, take
 h=0.2.
 Program:
@@ -79,6 +92,10 @@ OUTPUT:
 trapezoidal(f,0.2,1.4,6)
 Integration by trap is: 2.16
 2.5
+
+
+
+
 Q.5 Evaluate ∫0 𝑒 𝑥 𝑑𝑥 using trapezoidal rule, take h=0.5.
 Program:
 def trapezoidal(f, a, b, n):
